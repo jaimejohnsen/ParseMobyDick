7851cee114fe53c81c4f6b1fb5aa140d3f29165d
jaimejohnsen <35205839+jaimejohnsen@users.noreply.github.com>
2022-06-23 13:12:41 -0400

    Add files via upload

--- txtparse.py
+++ txtparse.py
@@ -5,4 +5,9 @@ import urllib.request
   
 data = urllib.request.urlopen('https://gist.githubusercontent.com/StevenClontz/4445774/raw/1722a289b665d940495645a5eaaad4da8e3ad4c7/mobydick.txt') # it's a file like object and works just like a file
 for line in data: # files are iterable
-    print (line)
+    #print (line)
+
+    read_data = data.read()
+    per_word = read_data.split()
+
+print('Total Words:', len(per_word))
